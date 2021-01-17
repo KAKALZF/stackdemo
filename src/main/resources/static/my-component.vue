a5341d0a2bd1436fdd089009a22d10db5ede2956
KAKALZF <575800503@qq.com>
2021-01-17 18:06:21 +0800

    springboot vue 前后端不分离

---
+++ src/main/resources/static/my-component.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        Hello {{who}}
+        <h1>test</h1>
+        <el-button type="primary">查询</el-button>
+    </div>
+
+</template>
+
+<script>
+    module.exports = {
+        data: function () {
+            return {
+                who: 'world'
+            }
+        }
+    }
+</script>
+
+<style>
+    .hello {
+        background-color: #ffe;
+    }
+</style>
