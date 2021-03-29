414234ebd1f451c9a7764a09f3ad0ffb0daad325
KAKALZF <575800503@qq.com>
2021-03-29 19:05:06 +0800

    springsecurity

--- src/main/java/com/ample16/stackdemo/controller/TestController.java
+++ src/main/java/com/ample16/stackdemo/controller/TestController.java
@@ -3,19 +3,17 @@ package com.ample16.stackdemo.controller;
 import com.ample16.stackdemo.service.DemoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/hello")
 public class TestController {
     @Autowired
     private DemoService demoService;
 
-    @GetMapping("/kaka")
-    public String test(@RequestParam("name") String name) {
-        return "hello world" + name;
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
     }
 
 }
